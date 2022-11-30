b1085f7473d82661294c5f11c6b04f6a2a0c16d4
Arnau Castellano <arcascb2001@gmail.com>
2022-11-30 11:27:39 +0100

    Update inst.sh

--- docker/inst.sh
+++ docker/inst.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd mmf/
 
-pip install --editable .
-pip uninstall -y torch torchvision torchaudio
-pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
-pip install pytorch-lightning==1.6.0
-pip install future
-pip install numpy==1.20.3 --upgrade
-pip install pytest
+python -m pip install --editable .
+python -m pip uninstall -y torch torchvision torchaudio
+python -m pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
+python -m pip install pytorch-lightning==1.6.0
+python -m pip install future
+python -m pip install numpy==1.20.3 --upgrade
+python -m pip install pytest
 
